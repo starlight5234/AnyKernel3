6ec05c1dc5d2d71478236f6e0e2f7cd38f52e53d
starlight5234 <ifist2834@gmail.com>
2020-05-11 13:15:16 +0000

    Add a nice flash screen message

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Unitrix Kernel by Starlight @star2834 Telegram
+kernel.string=Unitrix Kernel by Starlight @star2834 TG
 do.devicecheck=1
 do.modules=0
 do.systemless=1
